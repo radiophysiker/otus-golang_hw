163e16ec90e7ca957c08835a85bc0cee8aa43857
radiophysiker <bolshakovdmitry@bk.ru>
2024-08-23 04:52:29 +0300

    hw3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,31 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	words := strings.Fields(str)
+	mapWords := map[string]int{}
+	for _, el := range words {
+		if el != "" {
+			mapWords[el]++
+		}
+	}
+	result := make([]string, 0, len(mapWords))
+	for key := range mapWords {
+		result = append(result, key)
+	}
+	sort.Slice(result, func(i, j int) bool {
+		if mapWords[result[i]] == mapWords[result[j]] {
+			return result[i] < result[j]
+		}
+		return mapWords[result[i]] > mapWords[result[j]]
+	})
+
+	if len(result) > 9 {
+		return result[:10]
+	}
+	return result
 }
